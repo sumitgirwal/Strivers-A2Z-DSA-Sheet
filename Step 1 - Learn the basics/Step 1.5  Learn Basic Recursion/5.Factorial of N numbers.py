919bbec1685e0163333792dbd8b1a6fb8da23eb4
Sumit <sumitgirwals@gmail.com>
2023-01-05 14:19:25 +0530

    Added step 1.5 5-6 Q

--- Step 1 - Learn the basics/Step 1.5  Learn Basic Recursion/5.Factorial of N numbers.py
+++ Step 1 - Learn the basics/Step 1.5  Learn Basic Recursion/5.Factorial of N numbers.py
@@ -1,1 +1,2 @@
 # https://bit.ly/3QugUVo
+# https://practice.geeksforgeeks.org/problems/reverse-a-string/1
--- Step 1 - Learn the basics/Step 1.5  Learn Basic Recursion/6.Reverse an array.py
+++ Step 1 - Learn the basics/Step 1.5  Learn Basic Recursion/6.Reverse an array.py
@@ -1,1 +1,14 @@
 # https://bit.ly/3SRqzXU
+
+# https://practice.geeksforgeeks.org/problems/reverse-a-string/1
+
+string reverseWord(string str){
+  int start = 0;
+  int end = str.length()-1;
+  while(start<=end){
+      swap(str[start], str[end]);
+      start++;
+      end--;
+  }
+  return str;
+}
